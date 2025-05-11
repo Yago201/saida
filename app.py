1b86cc7f4d65c007e6af1a5fc0622ea0c0c049fd
Yago201 <hyagomattos2005@gmail.com>
2025-05-11 19:20:01 -0400

    https://github.com/Yago201/saida

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-# backend/app.py
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes.ponto import ponto_bp
 import os
@@ -7,12 +6,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates')
 CORS(app)
 
-# Registrar blueprints
+# Rotas da API
 app.register_blueprint(ponto_bp, url_prefix='/api')
+
+# Rota principal → admin.html
+@app.route('/')
+def admin():
+    return send_from_directory('templates', 'admin.html')
+
+# Rota /funcionario → index.html
+@app.route('/funcionario')
+def funcionario():
+    return send_from_directory('templates', 'index.html')
 
 if __name__ == '__main__':
     porta = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=porta)
+
